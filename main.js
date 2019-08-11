7bf8e96f684334acda287a6168866aee3406391b
Cake Nopika <cakeowo@protonmail.com>
2019-08-10 23:21:39 -0400

    Hidden the token bois

--- main.js
+++ main.js
@@ -10,5 +10,3 @@ client.on('message', msg => {
     msg.reply(`Candyland by CakeAwoo and Jellyfish Epik :) OwO `);
   }
 });
-
-client.login('NjA5OTM4NzE4MDM1NTQyMDc2.XU-AWA.6cUxA-uslnyjVsxvhEvZXpqYHqo');
